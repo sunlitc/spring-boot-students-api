a0bd0621b7ea48bad1cdb0edc98d987c0a4c1eb9
Samir Prakash <samir.prakash@toyotaconnected.com>
2017-03-26 23:58:00 -0500

    Refactored Dao to DaoImpl

---
+++ src/main/java/com/tc/sp/springboot/Dao/FakeStudentDaoImpl.java
@@ -0,0 +1,52 @@
+package com.tc.sp.springboot.Dao;
+
+import com.tc.sp.springboot.Entity.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class FakeStudentDaoImpl implements StudentDao {
+
+    private static Map<Integer, Student> students;
+
+    static {
+        students = new HashMap<Integer, Student>() {
+            {
+                put(1, new Student(1, "Samir", "CE"));
+                put(2, new Student(2, "Aashi", "MBA"));
+                put(3, new Student(3, "Pandey", "IT"));
+            }
+        };
+    }
+
+    @Override
+    public Collection<Student> getAllStudents() {
+        return this.students.values();
+    }
+
+    @Override
+    public Student getStudentById(int id) {
+        return this.students.get(id);
+    }
+
+    @Override
+    public void deleteStudentById(int id) {
+        this.students.remove(id);
+    }
+
+    @Override
+    public void updateStudent(Student student) {
+        Student s = this.students.get(student.getId());
+        s.setName(student.getName());
+        s.setCourse(student.getCourse());
+        this.students.put(student.getId(), student);
+    }
+
+    @Override
+    public void addStudent(Student student) {
+        this.students.put(student.getId(), student);
+    }
+}
--- src/main/java/com/tc/sp/springboot/Dao/StudentDao.java
+++ src/main/java/com/tc/sp/springboot/Dao/StudentDao.java
@@ -1,47 +1,17 @@
 package com.tc.sp.springboot.Dao;
 
 import com.tc.sp.springboot.Entity.Student;
-import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
-@Repository
-public class StudentDao {
+public interface StudentDao {
+    Collection<Student> getAllStudents();
 
-    private static Map<Integer, Student> students;
+    Student getStudentById(int id);
 
-    static {
-        students = new HashMap<Integer, Student>() {
-            {
-                put(1, new Student(1, "Samir", "CE"));
-                put(2, new Student(2, "Aashi", "MBA"));
-                put(3, new Student(3, "Pandey", "IT"));
-            }
-        };
-    }
+    void deleteStudentById(int id);
 
-    public Collection<Student> getAllStudents() {
-        return this.students.values();
-    }
+    void updateStudent(Student student);
 
-    public Student getStudentById(int id) {
-        return this.students.get(id);
-    }
-
-    public void deleteStudentById(int id) {
-        this.students.remove(id);
-    }
-
-    public void updateStudent(Student student) {
-        Student s = this.students.get(student.getId());
-        s.setName(student.getName());
-        s.setCourse(student.getCourse());
-        this.students.put(student.getId(), student);
-    }
-
-    public void addStudent(Student student) {
-        this.students.put(student.getId(), student);
-    }
+    void addStudent(Student student);
 }
