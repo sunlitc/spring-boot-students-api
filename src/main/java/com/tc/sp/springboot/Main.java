bc482d9ef90eac3295e601bc1e0d34785472c562
Samir Prakash <samir.prakash@toyotaconnected.com>
2017-05-22 13:55:21 -0500

    Updated main execution

--- src/main/java/com/tc/sp/springboot/Main.java
+++ src/main/java/com/tc/sp/springboot/Main.java
@@ -1,10 +1,21 @@
 package com.tc.sp.springboot;
 
+import com.tc.sp.springboot.config.ServerProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner{
+
+    @Autowired
+    private ServerProperties serverProperties;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(serverProperties);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
