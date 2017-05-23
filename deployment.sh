d4e689258e0771a99e61ed117b1c8828f4f95e03
Samir Prakash <samir.prakash@toyotaconnected.com>
2017-05-23 15:55:43 -0500

    Added bash file

---
+++ deployment.sh
@@ -0,0 +1,11 @@
+docker rmi students:dev
+
+docker build -t eastacr.azurecr.io/students:dev .
+
+docker login -u eastacr -p XS/Xr98wH=7=O+/j1w/=7iw2yWxdeRTI eastacr.azurecr.io
+
+docker push eastacr.azurecr.io/student:dev
+
+kubectl delete -f deployment.yml
+
+kubectl create -f deployment.yml
