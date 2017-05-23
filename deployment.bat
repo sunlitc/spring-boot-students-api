55cfb80aae669151bc3b42f5d647a8ab4ca0c6c2
Samir Prakash <samir.prakash@toyotaconnected.com>
2017-05-23 16:17:52 -0500

    renamed image

--- deployment.bat
+++ deployment.bat
@@ -4,7 +4,7 @@ docker build -t eastacr.azurecr.io/students:dev .
 
 docker login -u eastacr -p XS/Xr98wH=7=O+/j1w/=7iw2yWxdeRTI eastacr.azurecr.io
 
-docker push eastacr.azurecr.io/student:dev
+docker push eastacr.azurecr.io/students:dev
 
 kubectl delete -f deployment.yml
 
