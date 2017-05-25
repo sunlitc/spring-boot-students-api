c69aebd5d3af2edbed065d0cb299e4a094d054ec
Samir Prakash <samir.prakash@toyotaconnected.com>
2017-05-25 13:17:38 -0500

    Receiver implementation done

---
+++ src/main/java/com/tc/sp/springboot/amqp/Receiver.java
@@ -0,0 +1,20 @@
+package com.tc.sp.springboot.amqp;
+
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+@Component
+public class Receiver {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+}
