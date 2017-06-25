b140e0e804d06927f7e8b135673b49784ce7d3ae
Samir Prakash <Samir.Prakash@gmail.com>
2017-06-25 15:10:00 -0500

    Added in-memory security

---
+++ src/main/java/com/tc/sp/springboot/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.tc.sp.springboot.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/students/*")
+                .hasRole("USER")
+                .and()
+                .formLogin();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("samir")
+                .password("password")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("secret")
+                .roles("USER", "ADMIN");
+    }
+}
